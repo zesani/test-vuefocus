d5080d0c85763a2918223284fb3f0b61acf3bb5f
zesani <7sin@outlook.co.th>
2016-11-09 13:47:33 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="">
+    <button type="button" name="button" @click="s=!s"></button>
+    <input type="text" v-focus="true" v-show="s">
+  </div>
+</template>
+
+<script>
+import { focus } from 'vue-focus'
+export default {
+  directives: { focus: focus },
+  data () {
+    return {
+      s: true
+    }
+  },
+  computed: {},
+  mounted () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+</style>
